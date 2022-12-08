c02152e303bbe83071d24e218cabaaf8d807b7c5
Alex V <alex.v@nxrub.pw>
2022-12-08 15:25:51 -0300

    Fetch Base

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+const url = "https://platzi-avo.vercel.app/api/avo";
+const appNode = document.getElementById("app");
+/* WEB API
+    1. Conectar al servidor
+    2. Procesar la respuesta, y convertir dicho valor a JSON
+    3. JSON -> DATA -> Renderizar info browser
+*/
+window
+	.fetch(url)
+	.then((response) => response.json())
+	.then((responseParsed) => {
+		const allItems = [];
+		responseParsed.data.forEach((item) => {
+			const image = document.createElement("img");
+			image.src = `https://platzi-avo.vercel.app/${item.image}`;
+			const title = document.createElement("h2");
+			title.textContent = item.name;
+			const price = document.createElement("div");
+			price.textContent = item.price;
 
-console.log('Happy hacking :)')
+			const container = document.createElement("div");
+			container.append(image, title, price);
+			allItems.push(container);
+		});
+		appNode.append(...allItems);
+	});
